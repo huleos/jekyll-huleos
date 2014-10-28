e34423c97625a201c3e88c395fe78819a01b1a65
Julio Cesar Montenegro <creativo@686studio.com>
2014-10-28 16:35:40 -0700

    add update lightcss

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
-$( document ).ready(function() {
-	// Write your code
+$(document).ready(function(){
+  // Write your code
+
+  // Navigation
+  $(".navbar-toggle").click(function(){
+    $('.navbar-collapse').toggleClass("open");
+  });
+  
+  $(".navbar-collapse li a").click(function(){
+      $('.navbar-collapse').removeClass("open");
+  });
+
 });
